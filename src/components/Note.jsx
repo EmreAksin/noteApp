42d9d8de80930f6fad638d4e4c94bca9e0495adf
Emre Aksın <devemreaksin@gmail.com>
2023-09-15 15:43:44 +0300

    second commit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ function Note({ title, text, onDelete }) {
   return (
     <div className="border border-gray-300 p-2 m-2 rounded-md w-full bg-orange-700">
       <h1 className="font-extrabold p-3 overflow-wrap break-word bg-orange-900 rounded-xl">
-        {title}
+        {title}{" "}
       </h1>
       <div className="p-3 w-full overflow-wrap break-word  bg-orange-800 rounded-xl m-2 ">
         <p className="">{text}</p>
